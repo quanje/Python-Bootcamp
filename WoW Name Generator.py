32906633430f37f67346a713e246b792eb70592b
Quanje Hill <103902762+quanje@users.noreply.github.com>
2023-09-02 20:35:55 -0400

    Add files via upload

---
+++ WoW Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the name your WoW Character Generator")
+Color = input("What is your favorite color?\n")
+Hero = input("What is your favorite Super Hero?\n")
+print("Character name should be " + Color + " " + Hero)
