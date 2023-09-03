8aff8685a72de15e50ed74fb4dc4e3af46072f87
Quanje Hill <103902762+quanje@users.noreply.github.com>
2023-09-02 21:09:39 -0400

    Update Pizza Delivery Automation.py

--- Pizza Delivery Automation.py
+++ Pizza Delivery Automation.py
@@ -1,9 +1,9 @@
-# 🚨 Don't change the code below 👇
+#Pizza Delievery
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
-# 🚨 Don't change the code above 👆
+
 
 #Write your code below this line 
 
