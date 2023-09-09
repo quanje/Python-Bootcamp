9d567acc278d1788d1dded389204ba70030f4607
Quanje Hill <103902762+quanje@users.noreply.github.com>
2023-09-08 21:50:36 -0400

    Add files via upload

---
+++ import_Module.py
@@ -0,0 +1,20 @@
+# # import random
+# # import my_module
+# # random_int = random.randint(1,10)
+# # print(random_int)
+# import random
+
+# random_float = random.random() * 5
+# print(random_float)
+
+# love_score = random.randint(1,100)
+# print(f"Your love score is {love_score}")
+
+import random
+
+random = random.randint(0,1)
+
+if random == 0:
+ print("tails")
+else:
+  print("heads")
